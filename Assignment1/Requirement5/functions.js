5af964b0fdfd41e4631b11bfb7a40beee85d8990
pravin nandankar <pravinnandankar03@gmail.com>
2024-02-12 13:58:13 +0530

    Requirement 5 updated

--- Assignment1/Requirement5/functions.js
+++ Assignment1/Requirement5/functions.js
@@ -1,57 +1,16 @@
-const JsonData = [
-    {
-        "name": "Amit",
-        "city": "Mumbai",
-        "skills": [
-            "Actng",
-            "Singing",
-            "Dancing"
-        ],
-        "vehicle": {
-            "model": "BMW",
-            "year": 2021
-        }
-    },
-    {
-        "name": "Rahul",
-        "city": "Jaipur",
-        "skills": [
-            "Swiming",
-            "Walking",
-            "Dancing"
-        ],
-        "vehicle": {
-            "model": "Scoda",
-            "year": 2019
-        }
-    },
-    {
-        "name": "Vijay",
-        "city": "Pune",
-        "skills": [
-            "Sarcasm",
-            "Debate",
-            "RTI"
-        ],
-        "vehicle": {
-            "model": "M80",
-            "year": 1999
-        }
-    }
-];
-  
-  const mappedArray = JsonData.map(item => (
-    { name: item.name,
-     year: item.vehicle.year,
-     skills : item.skills.map((s) => s ),
-     model: item.vehicle.model
-    }));
-  
-  console.log("Mapped Array:");
-  console.log(mappedArray);
-  
-  const filteredArray = JsonData.filter(item => item.vehicle.year > 2020);
-  
-  console.log("\nFiltered Array Year more than 2020:");
-  console.log(filteredArray);
-  
+const response = require('./response.json');
+
+const mappedArray = response.map(item => ({
+  name: item.name,
+  year: item.vehicle.year,
+  skills: item.skills.map(s => s),
+  model: item.vehicle.model
+}));
+
+console.log("Mapped Array:");
+console.log(mappedArray);
+
+const filteredArray = response.filter(item => item.vehicle.year > 2020);
+
+console.log("\nFiltered Array Year more than 2020:");
+console.log(filteredArray);
