e28107d8640c96a055cc8d8e3b0ba20055cdf614
pravin nandankar <pravinnandankar03@gmail.com>
2024-02-12 09:28:38 +0530

    Requirement 3 updated (node modules in gitignore)

--- Assignment1/Requirement3/ApiCallOffset.js
+++ Assignment1/Requirement3/ApiCallOffset.js
@@ -1,27 +1,54 @@
-function fetchPincodeData(offset) {
-    return fetch(`https://api.data.gov.in/resource/6176ee09-3d56-4a3b-8115-21841576b2f6?offset=${offset}`)
+const baseUrl = 'https://api.data.gov.in/resource/6176ee09-3d56-4a3b-8115-21841576b2f6';
+
+let recordList = [];
+
+const createRequestUrl = (offset) => {
+    const parameters = {
+        'api-key': '579b464db66ec23bdd000001cdd3946e44ce4aad7209ff7b23ac571b',
+        format: 'json',
+        offset,
+        limit: 10,
+        'filters[deliverystatus]': 'Delivery',
+        'filters[divisionname]': 'Nagpur City',
+    };
+    return baseUrl + '?' + new URLSearchParams(parameters);
+};
+
+const fetchData = (requestUrl) => {
+    console.log("Fetching data from:", requestUrl); 
+    return fetch(requestUrl)
         .then(response => {
             if (!response.ok) {
-                throw new Error('Failed to fetch pincode data');
+                throw new Error('Failed to fetch data');
             }
             return response.json();
         })
-        .then(data => data.results)
+        .then(data => {
+            console.log("Received data:", data); 
+            return data; 
+        })
         .catch(error => {
-            console.error('Error in data:', error);
-            return []; // Return an empty array to ensure Promise.all continues
+            console.error('Error fetching data:', error);
+            throw error; 
         });
-}
+};
 
-const promises = [
-    fetchPincodeData(31),
-    fetchPincodeData(51),
-    fetchPincodeData(61)
-];
+const processDataAndPrint = (dataArray) => {
+    recordList = dataArray.reduce((acc, data) => [...acc, ...data.records], []);
+    console.log(`Total records received: ${recordList.length}`);
+    const pincodeList = recordList.map(record => ({
+        pincode: record.pincode,
+        name: record.officename,
+    }));
+    console.log(pincodeList);
+};
 
-Promise.all(promises)
-    .then(results => {
-        const combinedData = [].concat(...results);
-        console.log(combinedData);
-    })
-    .catch(error => console.error('Error fetching pincode data:', error));
+const fetchSpecificOffsets = () => {
+    const offsets = [31, 51, 61];
+    const fetchPromises = offsets.map(offset => fetchData(createRequestUrl(offset)));
+    Promise.all(fetchPromises)
+        .then(processDataAndPrint)
+        .catch(error => console.error('Error fetching data:', error));
+};
+
+fetchSpecificOffsets();
